76174e38dd3ca0048ff5520c5c3a9019f0b9769f
João Orlando <joaoorlandojr7@gmail.com>
2022-09-29 18:18:02 -0300

    first commit

---
+++ seachallenge/src/main/java/com/seaproject/seachallenge/SeachallengeApplication.java
@@ -0,0 +1,13 @@
+package com.seaproject.seachallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeachallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeachallengeApplication.class, args);
+	}
+
+}
