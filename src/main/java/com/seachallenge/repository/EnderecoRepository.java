190f70d9f5d555d1f27f1f6135b6545aedaf6a72
João Orlando <joaoorlandojr7@gmail.com>
2022-09-30 10:43:58 -0300

    criando repositories

---
+++ src/main/java/com/seachallenge/repository/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package com.seachallenge.repository;
+
+import com.seachallenge.model.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
