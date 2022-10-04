c3f3872ee42b85da0c9e5c1cbebf010592bb960f
João Orlando <joaoorlandojr7@gmail.com>
2022-10-04 17:20:13 -0300

    trabalhador service

--- src/main/java/com/seachallenge/controller/TrabalhadorController.java
+++ src/main/java/com/seachallenge/controller/TrabalhadorController.java
@@ -1,18 +1,16 @@
 package com.seachallenge.controller;
 
 import com.seachallenge.model.Trabalhador;
-import com.seachallenge.repository.CargoRepository;
-import com.seachallenge.repository.SetorRepository;
 import com.seachallenge.repository.TrabalhadorRepository;
+import com.seachallenge.service.TrabalhadorService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/trabalhador")
@@ -21,12 +19,9 @@ public class TrabalhadorController {
 
     @Autowired
     private TrabalhadorRepository trabalhadorRepository;
-
+    
     @Autowired
-    private CargoRepository cargoRepository;
-
-    @Autowired
-    private SetorRepository setorRepository;
+    private TrabalhadorService trabalhadorService;
 
 
     @GetMapping
@@ -48,35 +43,22 @@ public class TrabalhadorController {
 
     @PostMapping
     public ResponseEntity<Trabalhador> post(@Valid @RequestBody Trabalhador trabalhador){
-        if(cargoRepository.existsById(trabalhador.getCargo().getId()) && setorRepository.existsById(trabalhador.getSetor().getId()))
-            return ResponseEntity.status(HttpStatus.CREATED)
-                    .body(trabalhadorRepository.save(trabalhador));
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    	
+        return trabalhadorService.cadastrarTrabalhador(trabalhador)
+        		.map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(resposta))
+    			.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+        		
     }
     @PutMapping
     public ResponseEntity<Trabalhador> put(@Valid @RequestBody Trabalhador trabalhador){
-        if (trabalhadorRepository.existsById(trabalhador.getId())){
-
-            if (cargoRepository.existsById(trabalhador.getCargo().getId()) && setorRepository.existsById(trabalhador.getSetor().getId()))
-                return ResponseEntity.status(HttpStatus.OK)
-                        .body(trabalhadorRepository.save(trabalhador));
-
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-
-        }
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    	return trabalhadorService.atualizarTrabalhador(trabalhador)
+        		.map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(resposta))
+    			.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
-        Optional<Trabalhador> trabalhador = trabalhadorRepository.findById(id);
-
-        if(trabalhador.isEmpty())
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-
-        trabalhadorRepository.deleteById(id);
+        trabalhadorService.deletarTrabalhador(id);
     }
 }
--- src/main/java/com/seachallenge/model/Trabalhador.java
+++ src/main/java/com/seachallenge/model/Trabalhador.java
@@ -7,9 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
